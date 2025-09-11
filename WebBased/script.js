849cd26b392cc4dfa2794db6ff90b0926e1bc4a0
farrel-11 <23502410011@student.prasetiyamulya.ac.id>
2025-09-11 15:45:26 +0700

    Pengumpulan Django Lab + Quiz kelas

---
+++ WebBased/script.js
@@ -0,0 +1,9 @@
+console.log("Hello, World! Welcome to JavaScript programming.");
+
+alert("This is from farrel");
+
+let rating = 5;
+
+if (rating === 5) {
+    console.log("Thank you for the rating!");
+}
